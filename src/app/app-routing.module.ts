80e265eb1379d66071bbac96839f65d50faf71e7
KevinZhou <1065438834@qq.com>
2023-06-21 18:29:21 +0800

    table-list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,30 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { LayoutComponent } from './pages/layout/layout.component';
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
+import { LayoutComponent } from './pages/layout/layout.component'
+import { TableComponent } from './pages/table/table.component'
+import { ListComponent } from './pages/table/list/list.component'
 
 const routes: Routes = [
   {
     path: 'pages',
     component: LayoutComponent,
-  },
-];
+    children: [
+      {
+        path: 'table',
+        component: TableComponent,
+        children: [
+          {
+            path: 'list',
+            component: ListComponent
+          }
+        ]
+      }
+    ]
+  }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+  exports: [RouterModule]
 })
 export class AppRoutingModule {}
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 import { PagesRoutingModule } from './pages-routing.module';
 import { LayoutComponent } from './layout/layout.component';
 import { ShareModule } from '../share/share.module';
+import { TableComponent } from './table/table.component';
+import { ListComponent } from './table/list/list.component';
 
 @NgModule({
-  declarations: [LayoutComponent],
+  declarations: [LayoutComponent, TableComponent, ListComponent],
   imports: [CommonModule, ShareModule, PagesRoutingModule],
 })
 export class PagesModule {}
